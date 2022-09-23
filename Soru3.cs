b29a207778afe2a857ddc60e474426af74a45ff7
ceereenkoc <111969447+ceereenkoc@users.noreply.github.com>
2022-09-23 18:25:01 +0300

    Ödev2-Soru3
    Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.

---
+++ Soru3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Soru3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList sesliHarf = new ArrayList();
+            Console.WriteLine("Lütfen bir cümle yazınız:");
+            char[] cümle = (Console.ReadLine()).ToCharArray();
+            foreach (var h in cümle)
+            {
+                if (h == 'a' || h == 'e'|| h == 'ı' ||h == 'i' ||h == 'o' ||h == 'ö' ||h == 'u' || h == 'ü')
+                {
+                    sesliHarf.Add(h);
+                }
+            }
+
+            sesliHarf.Sort();
+            foreach (var item in sesliHarf)
+                Console.Write(" " + item);             
+        }
+    }
+}
